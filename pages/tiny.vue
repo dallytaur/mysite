982e533c9ad52a073e656af0f6775bad35fd0138
BuildTools <unconfigured@null.spigotmc.org>
2020-03-22 21:01:34 +1030

    third try to fix tiny for matting

--- pages/tiny.vue
+++ pages/tiny.vue
@@ -7,76 +7,87 @@
           <div class="box">
             <h2 class="title is-3">welcome tiny viewer</h2>
             <p></p>
- <div>      
- <div style="text-align:right">   
-<br>
+            <div>
+              <div>
+                <div>
+                  <div style="text-align:right">
+                    <br>
 
- I am glad you spent the time to watch the video and click on my link as of now I have a few things that I can make for you
+                    I am glad you spent the time to watch the video and click on my link as of now I have a few things
+                    that
+                    I can make for you
 
-<br>
+                    <br>
 
- you can purchase a fully assembled base of the dragon puppet that you see in tiny's video assuming that is been released yet
+                    you can purchase a fully assembled base of the dragon puppet that you see in tiny's video assuming
+                    that
+                    is been released yet
 
-<br>
- I also sell the fursuit based used and such as the adorable opossum I also make custom basis if you wish
-<br>
- currently I sell them into materials petg or tpu
-<br>
-<br>
-</divstyle="text-align:left">
-  <img src="/img/base.jpg" alt="base" height="200">
-<div>  
-<img>
-<div>  
-</div>
- As of now I have three tiers.
-<br>
-<br>
- $75  for a pre-made base from an existing stock
-<br>
-<br>
+                    <br>
+                    I also sell the fursuit based used and such as the adorable opossum I also make custom basis if you
+                    wish
+                    <br>
+                    currently I sell them into materials petg or tpu
+                    <br>
+                    <br>
+                  </div>
 
- $125 for me to spend the time to design you a base based on your criteria, with the condition that your base once completed is free to people to buy for the tier above.
-<br>
-<br>
+                  <img src="/img/base.jpg" alt="base" height="200">
+                </div>
+                As of now I have three tiers.
+                <br>
+                <br>
+                $75 for a pre-made base from an existing stock
+                <br>
+                <br>
 
- $350  for a true one-of-a-kind base owned purely by you you even get to keep the file for personal use 
- 
-<br>
-<br>
+                $125 for me to spend the time to design you a base based on your criteria, with the condition that
+                your base once completed is free to people to buy for the tier above.
+                <br>
+                <br>
 
- +$50 if you opt for semi flexible tpu which has the advantage of making it more crush resistance as well as slightly softing compared to petg
-<br>
-<br>
-+$30  for  realistic base  it's a little out of my comfort zone but I can manage.
-<br>
-<br>
- all bases come with moving jaw as well as integrated hinge system feel free to opt out if you wish to not have moving jaw
-<br>
-<br>
- I also design custom bits and pieces for your design work if you need stuff like horns, mechanical parts, eye parts and so on you can also message me for custom things as well.
-<br>
-<br>
+                $350 for a true one-of-a-kind base owned purely by you you even get to keep the file for personal use
 
- my contacts as a listed
-<br>
-<br>
- discord Nadia mayer#6480
-<br> 
-Twitter alicetaur
-<br> 
-telegram alicetaur
-<br> 
-Instagram dallytaur
-<br>
-<br>
+                <br>
+                <br>
 
- feel free to message me on any platform if you're intereste with buying anything
-            <hr>
-            <p><nuxt-link to="/">← return home</nuxt-link></p>
+                +$50 if you opt for semi flexible tpu which has the advantage of making it more crush resistance as
+                well as slightly softing compared to petg
+                <br>
+                <br>
+                +$30 for realistic base it's a little out of my comfort zone but I can manage.
+                <br>
+                <br>
+                all bases come with moving jaw as well as integrated hinge system feel free to opt out if you wish to
+                not have moving jaw
+                <br>
+                <br>
+                I also design custom bits and pieces for your design work if you need stuff like horns, mechanical
+                parts, eye parts and so on you can also message me for custom things as well.
+                <br>
+                <br>
+
+                my contacts as a listed
+                <br>
+                <br>
+                discord Nadia mayer#6480
+                <br>
+                Twitter alicetaur
+                <br>
+                telegram alicetaur
+                <br>
+                Instagram dallytaur
+                <br>
+                <br>
+
+                feel free to message me on any platform if you're intereste with buying anything
+                <hr>
+                <p>
+                  <nuxt-link to="/">← return home</nuxt-link>
+                </p>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
     </section>
   </div>
 </template>
