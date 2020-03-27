f571ced33f84cf2aa12751244a82fdfc6aa42bc8
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 16:44:18 +1030

    add talk to me page

---
+++ pages/talktome.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <div class="content">
+          <div class="box">
+            <h2 class="title is-3">Contacts</h2>
+
+            <p>My Contact are as bellow I talk to a lot of people so if I dont get to you after a while feel free to
+              resend I'm pretty open and talk about what you like and I also rp.</p>
+            <h2 class="title"> contacts</h2>
+            <p><a href="https://t.me/alicetaur">Telegram alicetaur</a></p>
+            <p><a href=">https://www.instagram.com/dallytaur/">Instagram dallytaur</a></p>
+            <p><a href="https://twitter.com/alicetaur"> Twitter alicetaur</a></p>
+            <p></p> Discord Nadia Mayer#6480</p>
+
+            <p>
+              <nuxt-link to="/">← return home</nuxt-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
