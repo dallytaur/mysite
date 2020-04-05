330d6e6d8ac5a36467e04cc2d01e70b72e0ae31a
BuildTools <unconfigured@null.spigotmc.org>
2020-04-05 14:13:01 +0930

    edits

--- pages/gfc.vue
+++ pages/gfc.vue
@@ -2,23 +2,23 @@
   <div>
     <section class="section">
       <div class="container">
-        <h1 class="title is-1">FAQ</h1>
+        <h1 class="title is-1">group furry commission</h1>
         <div class="content">
           <div class="box">
-            <h2 class="title is-3">Can I draw your characters?</h2>
+            <h2 class="title is-3">tos and info</h2>
             <p>Thank you for finding the time to read this group commission.</p>
 
             <p>Our artist for the commission of February March is Cervelet (https://www.furaffinity.net/user/cervelet/) and has requested to pay 10 euros(11 US dollars) person</p>
             
             <p>will be using PayPal for management of payments as well email and telegram for updates.</p>
             
-            <p>the current theme of this drawing is "springtime at the park"</p>
+            <p>the current theme of this drawing is "quarrantine party"</p>
             
             <p>Before you fill out the form here is a few guidelines</p>
             
             <p>>there is about 60 slots available.</p>
             
-            <p>>This is an SFW commission to mean we must keep this clean.</p>
+            <p>>This is an SFW commissionso so we must keep this clean.</p>
             
             <p>>preferably furry characters only.</p>
             
@@ -30,7 +30,7 @@
             
             <p>>Any bad talk will result in consequences listed above but positive criticism is welcome.</p>
             
-            <p>the process of this commission will go as follows</p>
+            <h2 class="title is-3">the process of this commission will go as follows</h2>
             
             <p>1.Submit a request by this document</p>
             
@@ -42,13 +42,11 @@
             
             <p>There will be a second at it request email this will be your last chance to correct any errors in your character</p>
             
-            <p>Lastly the finished artwork piece will be sent by email and concluding the end of the project. Another one of these projects will be released between the months of June and July.</p>
+            <p>Lastly the finished artwork piece will be sent by email and concluding the end of the project. Another one of these projects will be released after this one</p>
+          
+            <p><a href="https://t.me/GroupFurryCommissionProject">Telegram channel links for updates</a></p>
             
-            <p>Telegram channel links for updates:<br />
-            https://t.me/GroupFurryCommissionProject</p>
-
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScnjTi30Ry0MPmcFj64g8U6pZiuC1lBEdHMnHJC5CYDbk-RNg/viewform?embedded=true" width="640" height="1250" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
-
+            <p><a href="https://forms.gle/Ckvqwb2UvWnLCBT2A">Click here to submit</a></p>
             <nuxt-link to="/">← return home</nuxt-link>
             </p>
           </div>
