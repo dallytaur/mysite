184e56b767d77dd9efcb31e68a103b8c661eb451
BuildTools <unconfigured@null.spigotmc.org>
2020-04-05 13:57:14 +0930

    added GFC page

---
+++ pages/gfc.vue
@@ -0,0 +1,59 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1 class="title is-1">FAQ</h1>
+        <div class="content">
+          <div class="box">
+            <h2 class="title is-3">Can I draw your characters?</h2>
+            <p>Thank you for finding the time to read this group commission.</p>
+
+            <p>Our artist for the commission of February March is Cervelet (https://www.furaffinity.net/user/cervelet/) and has requested to pay 10 euros(11 US dollars) person</p>
+            
+            <p>will be using PayPal for management of payments as well email and telegram for updates.</p>
+            
+            <p>the current theme of this drawing is "springtime at the park"</p>
+            
+            <p>Before you fill out the form here is a few guidelines</p>
+            
+            <p>>there is about 60 slots available.</p>
+            
+            <p>>This is an SFW commission to mean we must keep this clean.</p>
+            
+            <p>>preferably furry characters only.</p>
+            
+            <p>>while we are open to pretty much any type of fursona we reserve the right to reject your character.</p>
+            
+            <p>>If you request more than one character in this commission make sure you fill out the form again with your new character.</p>
+            
+            <p>>Any chargebacks will result in exclusion from this project and any future ones.</p>
+            
+            <p>>Any bad talk will result in consequences listed above but positive criticism is welcome.</p>
+            
+            <p>the process of this commission will go as follows</p>
+            
+            <p>1.Submit a request by this document</p>
+            
+            <p>2. Receive acceptance or rejection email as well as a broadcast on the management telegram channel. After receiving this email you will be linked to paying your commission and will automatically be ticked off on receiving payment. Failure to do so will exclude you from this commission and removed from the list.</p>
+            
+            <p>3. The artist will then begin the commission there would be progress update released by email and telegram channel.</p>
+            
+            <p>4. An email will be released for requesting edits of your character and you will be linked to another Google form like this one. followed by another round of progress updates.</p>
+            
+            <p>There will be a second at it request email this will be your last chance to correct any errors in your character</p>
+            
+            <p>Lastly the finished artwork piece will be sent by email and concluding the end of the project. Another one of these projects will be released between the months of June and July.</p>
+            
+            <p>Telegram channel links for updates:<br />
+            https://t.me/GroupFurryCommissionProject</p>
+
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScnjTi30Ry0MPmcFj64g8U6pZiuC1lBEdHMnHJC5CYDbk-RNg/viewform?embedded=true" width="640" height="1250" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
+
+            <nuxt-link to="/">← return home</nuxt-link>
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
