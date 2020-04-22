cdcff9deae72b9c71076c6df29bfb9b17064bae3
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 11:21:22 +0930

    fuck

--- pages/gfc.vue
+++ pages/gfc.vue
@@ -47,9 +47,9 @@
             <p><a href="https://t.me/GroupFurryCommissionChat">Telegram group for chat and talking with your peers</a></p>
 
             <h2 class="title is-3">Older rt</h2>
-            <p>This art was made by Missy Merlin <a href="https://twitter.com/MissyMerlin"twitter here</p>
+            <p>This art was made by Missy Merlin <a href="https://twitter.com/MissyMerlin"Twitter Here</a></p>
             <img src="/img/gfc1.png" alt="base" height="200">
-            <p>This art was made by cervelet(still as -<a href="https://www.furaffinity.net/user/cervelet/"FA</p>
+            <p>This art was made by cervelet(still a wip)<a href="https://www.furaffinity.net/user/cervelet/">FA here</a></p>
             <img src="/img/gfc2.png" alt="base" height="200">
 
             <nuxt-link to="/">← return home</nuxt-link>
