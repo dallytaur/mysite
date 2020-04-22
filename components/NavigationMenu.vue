da244a16f15a7b348816c89f01b001bff6f88503
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 11:23:02 +0930

    error

--- components/NavigationMenu.vue
+++ components/NavigationMenu.vue
@@ -12,7 +12,7 @@
       <b-navbar-item tag="nuxt-link" :to="{ name: 'buyinfo' }">Commissioning</b-navbar-item>
       <!--<b-navbar-item tag="nuxt-link" :to="{ name: 'gallery' }">Gallery</b-navbar-item>-->
       <b-navbar-item tag="nuxt-link" :to="{ name: 'faq' }">FAQ</b-navbar-item>
-      <b-navbar-item tag="nuxt-link" :to="{ name: 'talktome' }">Contact me</b-navbar-item>
+      <b-navbar-item tag="nuxt-link" :to="{ name: 'talktome' }">Contact Me</b-navbar-item>
       <!--<b-navbar-item tag="nuxt-link" :to="{ name: 'gfc' }">Group furry commission project</b-navbar-item>-->
     </template>
   </b-navbar>
